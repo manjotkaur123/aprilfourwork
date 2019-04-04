65000d2526ba009785b33c343f22dc1cff3a5b9e
Manjot Kaur <731846@student.tor.loc>
2019-04-04 18:54:50 -0400

    submit button

--- Site.Master.cs
+++ Site.Master.cs
@@ -13,5 +13,10 @@ namespace WebApplication2
         {
 
         }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // to do code here
+        }
     }
 }
